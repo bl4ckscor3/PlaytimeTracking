5daf93893b4bd81cf4246950d7c3753ce49131aa
bl4ckscor3 <donalddani9612@gmx.de>
2017-11-16 13:32:40 +0100

    fix #1 | replace spaces with tabs where necessary

--- index.php
+++ index.php
@@ -7,42 +7,42 @@
 				<?php require('pageheader.php');?>
 				<div id="placeholder"></div>
 				<div class="container fixedbar">
-                	<div class="jumbotron" id="searchotron" style="padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px">
-                    	<form id="search" action="search.php">
-                			<button class="btn btn-info btn-md" type="submit" style="float: left"><span class="glyphicon glyphicon-search"></span></button>
-                			<input id="search-input" class="form-control" name="search" type="text" placeholder="Search for SteamID/name" style="width: 20em">
-                    	</form>
-                    </div>
-                </div> 
-                <div class="table-responsive">
-                	<table class="table table-striped">
-                		<tr>
-                			<th>Last Name</th>
-                			<th>SteamID</th>
-                			<th>Tryhard Ratio</th>
+					<div class="jumbotron" id="searchotron" style="padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px">
+						<form id="search" action="search.php">
+							<button class="btn btn-info btn-md" type="submit" style="float: left"><span class="glyphicon glyphicon-search"></span></button>
+							<input id="search-input" class="form-control" name="search" type="text" placeholder="Search for SteamID/name" style="width: 20em">
+						</form>
+					</div>
+				</div> 
+				<div class="table-responsive">
+					<table class="table table-striped">
+						<tr>
+							<th>SteamID</th>
+							<th>Last Name</th>
+							<th>Tryhard Ratio</th>
 						</tr>
 						<?php
-    						$names = $mysql->query("SELECT * FROM names");
-    						$times = $mysql->query("SELECT * FROM times");
-    						$maps = $mysql->query("SELECT * FROM maps");
-    						
-    						while($name = $names->fetch_assoc())
-    						{
-    						    $id = $name['steamid'];
-    						    $lastname = $name['lastname'];?>
-    						    
-    						    <tr onclick="location.href='user.php?id=<?php echo $id;?>'" style="cursor: pointer">
-    						    	<td><?php echo $lastname;?></td>
-    						    	<td><?php echo $id;?></td>
-    						    	<td>N/A</td>
-    						    </tr>
-    					<?php }?>
-                	</table>
-                </div>
+							$names = $mysql->query("SELECT * FROM names");
+							$times = $mysql->query("SELECT * FROM times");
+							$maps = $mysql->query("SELECT * FROM maps");
+							
+							while($name = $names->fetch_assoc())
+							{
+								$id = $name['steamid'];
+								$lastname = $name['lastname'];?>
+								
+								<tr onclick="location.href='user.php?id=<?php echo $id;?>'" style="cursor: pointer">
+									<td><?php echo $id;?></td>
+									<td><?php echo $lastname;?></td>
+									<td>N/A</td>
+								</tr>
+						<?php }?>
+					</table>
+				</div>
 			</div>
 		</div>
 	</body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script>
 		$(window).scroll(function() {
 			if($(window).scrollTop() > 190)
--- pageheader.php
+++ pageheader.php
@@ -1,11 +1,11 @@
 <?php 
-    require('api.php');
-    $mysql = getMysql();
+	require('api.php');
+	$mysql = getMysql();
 ?>
 
 <div id="link">
 	<a href="index.php"><span id="linkSpan"></span></a>
-    	<h1><font face="Unicorn">Map Tracking</font></h1>
-    	<h3 class="centered">By bl4ckscor3 and Vauff</h3>
+	<h1><font face="Unicorn">Map Tracking</font></h1>
+	<h3 class="centered">By bl4ckscor3 and Vauff</h3>
 </div>
 <hr>
