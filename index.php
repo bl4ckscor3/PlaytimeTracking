1b2f5469049a8bdae6b1e2e1d17f79d79e9435e8
bl4ckscor3 <donalddani9612@gmx.de>
2017-11-19 22:28:48 +0100

    add playtime column

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
 						<tr>
 							<th>SteamID</th>
 							<th>Last Name</th>
+							<th>Total Playtime</th>
 							<th>Tryhard Ratio</th>
 						</tr>
 						<?php
@@ -41,30 +42,38 @@
 								$id = $name['steamid'];
 								$lastname = $name['lastname'];
 								$times = $mysql->query("SELECT * FROM times WHERE steamid=\"".$mysql->real_escape_string($id)."\"");
-								$totaltime = 0.0;
+								$minutes = 0.0;
 								$tryhardtime = 0.0;
 								$ratio = 0.0;
+								$hours = 0;
 
 								while($time = $times->fetch_assoc())
 								{
-									$totaltime += floatval($time['time']);
+									$minutes += floatval($time['time']);
 
 									if($mysql->query("SELECT * FROM maps WHERE map=\"".$mysql->real_escape_string($time['map'])."\"")->fetch_assoc()['tryhard'] == 1)
 										$tryhardtime += floatval($time['time']);
 								}
 								
-								if($totaltime != 0)
+								if($minutes != 0)
 								{
-									$ratio = round(($tryhardtime / $totaltime) * 10, PHP_ROUND_HALF_UP) / 10;
+									$ratio = round(($tryhardtime / $minutes) * 10, PHP_ROUND_HALF_UP) / 10;
 								}
 								else
 								{
 									$ratio = 0;
+								}
+								
+								while($minutes >= 60)
+								{
+								    $hours += 1;
+								    $minutes -= 60;
 								}?>
 								
 								<tr onclick="location.href='user.php?id=<?php echo $id;?>'" style="cursor: pointer">
 									<td><?php echo $id;?></td>
 									<td><?php echo $lastname;?></td>
+									<td><?php echo $hours."h:".$minutes."m"?></td>
 									<td><?php echo $ratio;?></td>
 								</tr>
 						<?php }?>
