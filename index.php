0be324567d70433b93330e54259d097f153e38c1
bl4ckscor3 <donalddani9612@gmx.de>
2017-11-19 01:52:46 +0100

    add pages | change button color | fix incorrect sizing of fixed bar

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
+<?php 
+	if(!isset($_GET['page']))
+	{
+		$_GET['page'] = 1;
+	}
+	
+	$page = $_GET['page'];
+?>
 <!DOCTYPE html>
 <html>
 	<?php require('head.php');?>
 	<body>
 		<div class="container" style="margin-left: auto; margin-right: auto;">
-			<div class="jumbotron">
+			<div class="jumbotron" id="parent">
 				<?php require('pageheader.php');?>
 				<div id="placeholder"></div>
-				<div class="container fixedbar">
-					<div class="jumbotron" id="searchotron" style="padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px">
+				<div class="container fixedbar" id="child">
+					<div class="jumbotron" style="padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px; border-radius: 0px;">
 						<form id="search" action="search.php">
-							<button class="btn btn-info btn-md" type="submit" style="float: left"><span class="glyphicon glyphicon-search"></span></button>
+							<button class="btn btn-warning btn-md" type="submit" style="float: left"><span class="glyphicon glyphicon-search"></span></button>
 							<input id="search-input" class="form-control" name="search" type="text" placeholder="Search for SteamID/name" style="width: 20em">
 						</form>
+						<?php require('pageselector.php');?>
 					</div>
-				</div> 
+				</div>
+				<h2>Player Times</h2><br>
 				<div class="table-responsive">
 					<table class="table table-striped">
 						<tr>
@@ -22,7 +32,9 @@
 							<th>Tryhard Ratio</th>
 						</tr>
 						<?php
-							$names = $mysql->query("SELECT * FROM names");
+							$max = 20 * $page;
+							$min = $max > 19 ? $max - 19 : 0;
+							$names = $mysql->query("SELECT * FROM names WHERE id>=".$mysql->real_escape_string($min)." AND id<=".$mysql->real_escape_string($max)." ORDER BY id ASC");
 							
 							while($name = $names->fetch_assoc())
 							{
@@ -41,7 +53,14 @@
 										$tryhardtime += floatval($time['time']);
 								}
 								
-								$ratio = round(($tryhardtime / $totaltime) * 10, PHP_ROUND_HALF_UP) / 10;?>
+								if($totaltime != 0)
+								{
+									$ratio = round(($tryhardtime / $totaltime) * 10, PHP_ROUND_HALF_UP) / 10;
+								}
+								else
+								{
+									$ratio = 0;
+								}?>
 								
 								<tr onclick="location.href='user.php?id=<?php echo $id;?>'" style="cursor: pointer">
 									<td><?php echo $id;?></td>
@@ -56,21 +75,29 @@
 	</body>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script>
+		var once = false;
+		
 		$(window).scroll(function() {
 			if($(window).scrollTop() > 190)
 			{
  				$(".fixedbar").addClass("fixed");
- 				$("#searchotron").css("width", "113%");
- 				$("#placeholder").addClass("container");
-				document.getElementById("placeholder").innerHTML = "<div class=\"jumbotron\" style=\"padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px\"><form><button class=\"btn btn-info btn-md\" type=\"submit\" style=\"float: left\"><span class=\"glyphicon glyphicon-search\"></span></button><input class=\"form-control\" name=\"search\" type=\"text\" placeholder=\"Search for Steam ID or last known name\" style=\"width: 20em\"></form></div>";
+ 				document.getElementById("placeholder").innerHTML = "<div class=\"jumbotron\" style=\"padding-bottom: 20px; padding-top: 20px; margin-bottom: 0px\"><form><button class=\"btn btn-info btn-md\" type=\"submit\" style=\"float: left\"><span class=\"glyphicon glyphicon-search\"></span></button><input class=\"form-control\" name=\"search\" type=\"text\" placeholder=\"Search for Steam ID or last known name\" style=\"width: 20em\"></form></div>";
+
+ 				if(!once)
+ 				{
+ 	 				$("#child").width($("#parent").width());
+ 	 				once = true;
+ 				}
 			}
 			else
 			{
 				$(".fixedbar").removeClass("fixed");
- 				$("#searchotron").css("width", "");
- 				$("#placeholder").removeClass("container");
 				document.getElementById("placeholder").innerHTML = "";
 			}
+		});
+
+		$(window).resize(function() {
+			$("#child").width($("#parent").width());
 		});
 	</script>
 </html>
---
+++ pageselector.php
@@ -0,0 +1,29 @@
+<h4 style="float: right; margin-top: -34px">
+	<?php echo $page > 1 ? "<a href=\"index.php".($page - 1 == 1 ? "" : "?page=".($page - 1))."\">" : ""?>
+		<button class="btn btn-warning<?php echo $page <= 1 ? " disabled" : "";?>"><span class="glyphicon glyphicon-arrow-left"></span></button>
+	<?php echo $page > 1 ? "</a>" : "";
+		
+		$mysql = getMysql();
+		$pages = $mysql->query('SELECT COUNT(id) AS id FROM names')->fetch_assoc()['id'] / 20;
+		$ceiled = ceil($pages);
+		
+		for($i = 1; $i <= $ceiled; $i++)
+		{
+			if($i == $page)
+			{
+				echo $i." ";
+			}
+			else if($i == 1)
+			{
+				echo "<a href=\"index.php\">".$i."</a> ";
+			}
+			else
+			{
+				echo "<a href=\"index.php?page=".$i."\">".$i."</a> ";
+			}
+		}
+		?>
+	<?php echo $page < $ceiled ? "<a href=\"index.php?page=".($page + 1)."\">" : ""?>
+	<button class="btn btn-warning<?php echo $page >= $ceiled ? " disabled" : "";?>"><span class="glyphicon glyphicon-arrow-right"></span></button>
+	<?php echo $page < $ceiled ? "</a>" : "";?>
+</h4>
