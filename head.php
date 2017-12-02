f8383e52da70204803107c3ec09cd0a95f34c8d8
bl4ckscor3 <donalddani9612@gmx.de>
2017-12-02 23:11:08 +0100

    rename project

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
 	<meta charset="UTF-8">
-	<title>Map Tracking</title>
+	<title>Playtime Tracking</title>
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/bootstrap-theme.css">
 	<link rel="stylesheet" href="css/maptracking.css">
--- pageheader.php
+++ pageheader.php
@@ -5,7 +5,7 @@
 
 <div id="link">
 	<a href="index.php"><span id="linkSpan"></span></a>
-	<h1><font face="Unicorn">Map Tracking</font></h1>
+	<h1><font face="Unicorn">Playtime Tracking</font></h1>
 	<h3 class="centered">By bl4ckscor3 and Vauff</h3>
 </div>
 <hr>
